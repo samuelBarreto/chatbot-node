8358d96bb52876d71577ef38a70fb9a835bcd166
samuel <samukacfc1@gmail.com>
2018-10-02 19:50:04 -0300

    chat v.01

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('./public'));
+
+const port = 3000;
+
+const assistant = new AssistantV1({
+  username: '43e13771-6c4e-457d-a8a3-aa6fbd238d3e',
+  password: '4BtT3Ih0A5hF',
+  url: 'https://gateway.watsonplatform.net/assistant/api',
+  version: '2018-02-16',
+});
+
+app.post('/conversation/', (req, res) => {
+  const { text, context = {} } = req.body;
+
+  const params = {
+    input: { text },
+    workspace_id: 'b7cf5de8-1731-492b-9ee5-62359acac8fa',
+    context,
+  };
+
+  assistant.message(params, (err, response) => {
+    if (err) res.status(500).json(err);
+
+    res.json(response);
+  });
+});
+
+app.listen(port, () => console.log(`Servidor rodando na port ${port}`));
---
+++ public/js/script.js
@@ -0,0 +1,57 @@
+const textInput = document.getElementById('textInput');
+const chat = document.getElementById('chat');
+
+let context = {};
+
+const templateChatMessage = (message, from) => `
+  <div class="from-${from}">
+    <div class="message-inner">
+      <p>${message}</p>
+    </div>
+  </div>
+  `;
+
+// Crate um elemento e acrescentar ao chat
+const InsertTemplateInTheChat = (template) => {
+  const div = document.createElement('div');
+  div.innerHTML = template;
+
+  chat.appendChild(div);
+};
+
+// Calling server and get the watson output
+//Chamando o servidor e obtenha a saída do watson
+const getWatsonMessageAndInsertTemplate = async (text = '') => {
+  const uri = 'http://localhost:3000/conversation/';
+
+  const response = await (await fetch(uri, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      text,
+      context,
+    }),
+  })).json();
+
+  context = response.context;
+
+  const template = templateChatMessage(response.output.text, 'watson');
+
+  InsertTemplateInTheChat(template);
+};
+
+textInput.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13 && textInput.value) {
+    //Envie a mensagem do usuário
+    getWatsonMessageAndInsertTemplate(textInput.value);
+
+    const template = templateChatMessage(textInput.value, 'user');
+    InsertTemplateInTheChat(template);
+
+    // Limpar caixa de entrada para mensagens adicionais
+    textInput.value = '';
+  }
+});
+
+
+getWatsonMessageAndInsertTemplate();
