6dacbbb1436fe952e3c99035967e3858c41e2575
Samuel Barreto de Oliveira <samukacfc1@gmail.com>
2018-10-05 13:17:16 -0300

    Configuração do Watson ibm
    Configuração do Watson ibm

--- controllers/assitente.js
+++ controllers/assitente.js
@@ -6,13 +6,13 @@ module.exports = function (app) {
 
     var assistant = new AssistantV1({
 
-        username: '43e13771-6c4e-457d-a8a3-aa6fbd238d3e',
+        username: '-------',
 
-        password: '4BtT3Ih0A5hF',
+        password: '-----------',
 
         url: 'https://gateway.watsonplatform.net/assistant/api',
 
-        version: '2018-02-16',
+        version: '',
 
     });
 
@@ -38,7 +38,7 @@ module.exports = function (app) {
 
             },
 
-            workspace_id: 'b7cf5de8-1731-492b-9ee5-62359acac8fa',
+            workspace_id: '--------',
 
             context,
 
